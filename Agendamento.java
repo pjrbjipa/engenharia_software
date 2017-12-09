64ed6e17c68edbdb1492413f5308632257ba9d15
Pedro Junior <pjrbjipa@gmail.com>
2017-12-08 20:56:41 -0400

    Commit 2 segunda alteração
    Nome

--- Agendamento.java
+++ Agendamento.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +7,7 @@ package regrasDeNegocio;
 
 /**
  *
-
+ * Pedro Junior
  */
 public class Agendamento {
     private int idRegistro; //número do Registro
